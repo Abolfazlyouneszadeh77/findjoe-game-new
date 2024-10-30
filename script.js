51b9f8ded43a8c703acecdc6a1c1a5918e659e7b
abolfazl <abolfazl.yuni@gmail.com>
2024-10-31 02:42:22 +0330

    menu-setting-script
    menu setting built

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 level1Img.onclick = function () {
-    menuBar.className = "hide";
-    map.className = "show";
+     menuBar.className = "hide";
+     map.className = "show";
 }
-settingButton.onclick = function () {
+
+pos=0;
+setting.onclick = function onSetting () {
+        if(pos==0){
+             menuSlider.style.filter="blur(2px);";
+             settingPage.style.top= "-5px";
+             settingPage.style.transition="1s";
+             pos=1;
+        }else if(pos==1){
+             menuSlider.style.filter="blur(0px)";
+             settingPage.style.top= "-492px";
+             settingPage.style.transition="1s";
+             pos=0;
+            }
+}
+closePage.onclick=function(){
+             menuSlider.style.filter="blur(0px)";
+             settingPage.style.top= "-492px";
+             settingPage.style.transition="1s";
+             pos=0;
+}
+
+
+
+
+shopButton.onclick = function () {
     alert("...");
 }
