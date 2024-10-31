e0758821aa611cbd330a1e095980dab6736d0fac
abolfazl <abolfazl.yuni@gmail.com>
2024-10-31 14:27:02 +0330

    script-update
    menu script updated by abolfazl

--- script.js
+++ script.js
@@ -1,32 +1,34 @@
-level1Img.onclick = function () {
+level1Img.onclick = ()=> {
      menuBar.className = "hide";
      map.className = "show";
 }
 
 pos=0;
-setting.onclick = function onSetting () {
-        if(pos==0){
-             menuSlider.style.filter="blur(2px);";
+function onsetting(){
+     if(pos==0){
+          //    menuSlider.style.filter="blur(2px);";
              settingPage.style.top= "-5px";
              settingPage.style.transition="1s";
              pos=1;
         }else if(pos==1){
-             menuSlider.style.filter="blur(0px)";
+          //    menuSlider.style.filter="blur(0px)";
              settingPage.style.top= "-492px";
              settingPage.style.transition="1s";
              pos=0;
             }
 }
-closePage.onclick=function(){
-             menuSlider.style.filter="blur(0px)";
-             settingPage.style.top= "-492px";
-             settingPage.style.transition="1s";
-             pos=0;
+setting.onclick = ()=> {
+     onsetting();
+}
+closePage.onclick=()=> {
+     onsetting();
 }
 
+shopButton.onclick =()=> {
+    alert("shopping closed");
+}
 
-
-
-shopButton.onclick = function () {
-    alert("...");
+backButtonMap.onclick=()=>{
+     map.className="hide";
+     menuBar.className="show";
 }
